b28a8a65981450466b3f2a477874bf6919ade0f7
w1llyVR <wvaleric@gmail.com>
2023-04-25 12:27:56 -0500

    new Component: Landing Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: LandingPageComponent},
+  {path: '', pathMatch:"full", redirectTo:'home'},
+  {path:'**', pathMatch: "full", redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
