65f64eb253cf10761cd855e975cc11503df1b855
Willy Valentin <67374479+w1llyVR@users.noreply.github.com>
2023-04-25 16:02:30 -0500

    Crud - cine

--- src/app/services/cine.service.ts
+++ src/app/services/cine.service.ts
@@ -5,5 +5,36 @@ import { Injectable } from '@angular/core';
 })
 export class CineService {
 
-  constructor() { }
+  elements: any[] =[
+     { id: 1,nombre: "Titanic", photo: "fotito.png", duracion: 2.34, genero:"terror"},
+     { id: 2,nombre: "Titanic1", photo: "fotito.png", duracion: 2.34, genero:"terror"}, 
+     { id: 3,nombre: "Titanic2", photo: "fotito.png", duracion: 2.34, genero:"terror"} 
+     ];
+
+   constructor(){} 
+   
+   add_element(element:any){
+   this.elements.push(element);
+   }
+
+   get_elements() {
+      return this.elements;
+   } 
+
+   update_element(element:any){
+      const index = this.elements.findIndex(e => e.id === element.id);
+      if (index !== -1)
+      {
+         this.elements[index] = element;
+      } 
+   } 
+
+  delete_element(element:any){
+      const index = this.elements.findIndex(e => e.id === element.id);
+      if (index !== -1)
+      {
+         this.elements.splice(index,1);
+      } 
+   } 
+
 }
